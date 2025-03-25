28e530973b04c31e7ccf2e8fca800721b096eb90
Sabihahussaini <sabihahussaini002@gmail.com>
2025-03-25 14:21:22 -0400

    Add files via upload

---
+++ LoginTest.java
@@ -0,0 +1,40 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+    }
+
+    @Test
+    public void validLoginTest() {
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("submit"));
+
+        username.sendKeys("student");
+        password.sendKeys("Password123");
+        loginButton.click();
+
+        WebElement successMsg = driver.findElement(By.tagName("h1"));
+        Assert.assertEquals(successMsg.getText(), "Logged In Successfully");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# automation-login-test
+
+# Automation Login Test
+
+This project demonstrates a Selenium automation script in Java to test a login functionality.
+
+## 🔧 Tools Used
+- Java
+- Selenium WebDriver
+- TestNG
+- ChromeDriver
+
+## 💻 Scenario
+- Open browser
+- Navigate to login page
+- Enter valid credentials
+- Click Login
+- Verify successful login message
+
+## ✅ How to Run
+1. Make sure you have Java, TestNG, and ChromeDriver installed
+2. Update `chromedriver` path in `LoginTest.java`
+3. Run using:
+```
+testng testng.xml
+```
